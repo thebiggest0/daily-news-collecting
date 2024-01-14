7c2e91a5a2331fa516555d5120b92129539e090d
Cai Chun Yan <caichunyan17@gmail.com>
2024-01-13 20:39:30 -0800

    Complete all functions in save.py

--- data_manage/save.py
+++ data_manage/save.py
@@ -2,6 +2,7 @@
 ADD A DOCSTRING
 """
 import json
+import re
 
 
 def write_json(json_file_path, data):
@@ -17,10 +18,34 @@ def read_json(json_file_path):
     return data
 
 
+def process_textfile():
+    location = '../data/textfile.txt'
+    with open(location, 'r') as files:
+        data = files.read().replace('\n}\n{', ',')
+    count = [0]
+
+    def add_id(match):
+        count[0] += 1
+        return '"' + str(count[0]) + '"'
+
+    # change format to dictionary style
+    data = re.sub(r'"#"', add_id, data)
+    data = data.replace("\n", '')
+    return data
+
+
+def textfile_to_json(data):
+    dictionary = json.loads(data)
+    with open('../data/test.json', 'w') as file:
+        json.dump(dictionary, file, indent=4)
+
+
 def main():
-    path = '../data/weather.json'
-    read_json(path)
-    write_json(path)
+    # path = '../data/weather.json'
+    # read_json(path)
+    # write_json(path)
+    data = process_textfile()
+    textfile_to_json(data)
 
 
 if __name__ == "__main__":
