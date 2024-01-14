227e19378702aec5194fbc3bf6f0dc53661e3d85
Cai Chun Yan <caichunyan17@gmail.com>
2024-01-13 22:22:26 -0800

    Update SQL config placeholder text and comments

--- database/SQL.py
+++ database/SQL.py
@@ -1,39 +1,70 @@
 import mysql.connector
 from data_manage import save
+import json
 
 
-def sql_access():
+def sql_access_cloud():
     config = {
-        'user': 'your_username',
-        'password': 'your_password',
-        'host': 'host_address',
-        'database': 'database_name',
-        'port': 3306
+        'user': 'your_username',  # Replace with your RDS username, often 'admin'
+        'password': 'your_password',  # Replace with your RDS password
+        'host': 'your_rds_instance_endpoint',  # Replace with your RDS instance endpoint
+        'database': 'your_database_name',  # Replace with your database name
+        'port': 3306  # Replace with your RDS instance port, default is 3306 for MySQL
+    }
+
+
+def sql_access_local():
+    config = {
+        'user': 'local_user',  # Replace with your local database username
+        'password': 'local_password',  # Replace with your local database password
+        'host': 'localhost',  # Typically 'localhost' for a local database
+        'database': 'local_database_name',  # Replace with the name of your local database
+        'port': 3306  # Replace with the port your local database is using (3306 is common for MySQL)
     }
     return config
 
 
-def sql_insert_data(config, data):
+def sql_write_data(config, data):
     # Connect to the MySQL database
-    conn = mysql.connector.connect(**config)
-    cursor = conn.cursor()
+    connect = mysql.connector.connect(**config)
+    cursor = connect.cursor()
 
     # Insert data into the table
     for entry in data:
         cursor.execute('''
-            INSERT INTO news (news_id, title, category, length)
-            VALUES (DEFAULT, %s, %s, %s)
-        ''', (data[entry]['title'], data[entry]['category'], data[entry]['length']))
+            INSERT INTO news (news_id, title, category, length, summary, url)
+            VALUES (DEFAULT, %s, %s, %s, %s, %s)
+        ''', (data[entry]['title'], data[entry]['category'], data[entry]['length'], data[entry]['summary'], data[entry]['url']))
 
     # # Commit the transactions
-    conn.commit()
+    connect.commit()
 
     # Close the connection
     cursor.close()
-    conn.close()
+    connect.close()
+
+
+def sql_read_data(config):
+    connect = mysql.connector.connect(**config)
+    cursor = connect.cursor(dictionary=True)
+
+    cursor.execute(f"SELECT * FROM news")
+
+    rows = cursor.fetchall()
+    with open('../data/sql_data.json', 'w') as file:
+        json.dump(rows, file, indent=4)
+
+    cursor.close()
+    connect.close()
 
 
 def main():
-    configuration = sql_access()
+    # create/read data from relational table
+    configuration = sql_access_local()
     data_news = save.read_json('../data/news.json')
-    sql_insert_data(configuration, data_news)
+    sql_write_data(configuration, data_news)
+    # sql_read_data(configuration)
+
+
+if __name__ == '__main__':
+    main()
