8d9038e2f9695fd12d9d6283fd2da7ae5203a571
Cai Chun Yan <caichunyan17@gmail.com>
2024-01-13 21:54:21 -0800

    Implement final main() function to execute program

---
+++ app/main.py
@@ -0,0 +1,29 @@
+"""
+ADD A DOCSTRING
+"""
+from search import search
+from search import match_image
+from data_manage import save
+from database import SQL
+
+
+def main():
+    # # get data from links
+    links = search.acquire_links()
+    for link in links:
+        search.acquire_data(link)
+        match_image.transfer_data()
+
+    # process text file into json
+    data = save.process_textfile()
+    save.textfile_to_json(data)
+    save.clear_textfile()
+
+    # writing to database
+    config = SQL.sql_access()
+    data_news = save.read_json('../data/news.json')
+    SQL.sql_write_data(config, data_news)
+
+
+if __name__ == "__main__":
+    main()
