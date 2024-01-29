24a907fb9a6d6958ee081f5ee9266da3433a70fd
Cai Chun Yan <caichunyan17@gmail.com>
2024-01-29 08:30:03 -0800

    Implement save link functions

---
+++ find_news/get_links.py
@@ -0,0 +1,21 @@
+"""
+ADD A DOCSTRING
+"""
+from data_manage import save
+
+def get_links():
+    links = []
+    for _ in range(10):
+        link = 'test'
+        links.append(link)
+
+    return links
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
