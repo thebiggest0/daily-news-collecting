c289b2453468dc4bbf979ef4f5dcef1be7d4745c
Cai Chun Yan <caichunyan17@gmail.com>
2024-02-01 22:03:24 -0800

    Implement data to table functions

--- database/SQL.py
+++ database/SQL.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from data_manage import save
 import json
+import re
 
 
 def sql_access_cloud():
@@ -24,7 +25,7 @@ def sql_access_local():
     return config
 
 
-def sql_write_data(config, data):
+def sql_write_data_old(config, data):
     # Connect to the MySQL database
     connect = mysql.connector.connect(**config)
     cursor = connect.cursor()
@@ -35,6 +36,52 @@ def sql_write_data(config, data):
             INSERT INTO news (news_id, title, category, length, summary, url)
             VALUES (DEFAULT, %s, %s, %s, %s, %s)
         ''', (data[entry]['title'], data[entry]['category'], data[entry]['length'], data[entry]['summary'], data[entry]['url']))
+
+    # # Commit the transactions
+    connect.commit()
+
+    # Close the connection
+    cursor.close()
+    connect.close()
+
+
+def sql_write_data_new(config, entry, category, popular):
+    # Connect to the MySQL database
+    connect = mysql.connector.connect(**config)
+    cursor = connect.cursor()
+
+    # Insert data into the table
+    def strip_non_numbers(input_string):
+        return re.sub(r'\D', '', input_string)
+
+    length = ''
+    for i in range(len(entry['content']) - 1, -1, -1):
+        if entry['content'][i] == ']':
+            for j in range(i - 1, -1, -1):
+                if entry['content'][j] == '[':
+                    break
+                length += entry['content'][j]
+            length = strip_non_numbers(length)
+            length = int(length[::-1])
+            break
+
+    try:
+        cursor.execute('''
+            INSERT INTO api_news (news_id, title, category, author, length, description, publishDate, url, popular, source)
+            VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ''', (entry['title'], category, entry['author'], length, entry['description'], entry['publishedAt'][:10], entry['url'], popular, entry['source']['name']))
+    except mysql.connector.Error:
+        pass
+
+    if popular:
+        try:
+            cursor.execute('''
+                INSERT INTO popular_news (news_id, title, category, author, length, description, publishDate, url, popular, source)
+                VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ''', (entry['title'], category, entry['author'], length, entry['description'], entry['publishedAt'][:10],
+                  entry['url'], popular, entry['source']['name']))
+        except mysql.connector.Error as e:
+            print(e)
 
     # # Commit the transactions
     connect.commit()
@@ -62,7 +109,7 @@ def main():
     # create/read data from relational table
     configuration = sql_access_local()
     data_news = save.read_json('../data/news.json')
-    sql_write_data(configuration, data_news)
+    sql_write_data_new(configuration, data_news)
     # sql_read_data(configuration)
 
 
