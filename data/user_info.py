34fab6e16fefd72e4ab69fdbc07387ee8e6431e8
Cai Chun Yan <caichunyan17@gmail.com>
2024-02-08 12:33:48 -0800

    Add comments to describe input field

--- data/user_info.py
+++ data/user_info.py
@@ -1,14 +1,14 @@
-NEWSAPI_KEY = "your_api_key"
+NEWSAPI_KEY = "your_api_key"  # Replace with your API key from newsAPI
 
-CLOUD_LOCAL_USER = "your_username"
-CLOUD_LOCAL_PASSWORD = "your_password"
-CLOUD_HOST = "your_rds_instance_endpoint"
-CLOUD_DATABASE = "your_database_name"
-CLOUD_PORT = 3306
+CLOUD_USER = "your_username"  # Replace with your RDS username, often 'admin'
+CLOUD_PASSWORD = "your_password"  # Replace with your RDS password
+CLOUD_HOST = "your_rds_instance_endpoint"  # Replace with your RDS instance endpoint
+CLOUD_DATABASE = "your_database_name"  # Replace with your database name
+CLOUD_PORT = 3306  # Replace with your RDS instance port, default is 3306 for MySQL
 
-LOCAL_USER = "local_username"
-LOCAL_PASSWORD = "local_password"
-LOCAL_HOST = "localhost"
-LOCAL_DATABASE = "local_database_name"
-LOCAL_PORT = 3306
+LOCAL_USER = "local_username"  # Replace with your local database username
+LOCAL_PASSWORD = "local_password"  # Replace with your local database password
+LOCAL_HOST = "localhost"  # Typically 'localhost' for a local database
+LOCAL_DATABASE = "local_database_name"  # Replace with the name of your local database
+LOCAL_PORT = 3306  # Replace with the port your local database is using (3306 is common for MySQL)
 
