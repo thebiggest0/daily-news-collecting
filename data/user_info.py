02ac9c73746bac8a6ad9e92945ff19d91de410a4
Cai Chun Yan <caichunyan17@gmail.com>
2024-02-08 12:23:30 -0800

    Add user info template

---
+++ data/user_info.py
@@ -0,0 +1,14 @@
+NEWSAPI_KEY = "your_api_key"
+
+CLOUD_LOCAL_USER = "your_username"
+CLOUD_LOCAL_PASSWORD = "your_password"
+CLOUD_HOST = "your_rds_instance_endpoint"
+CLOUD_DATABASE = "your_database_name"
+CLOUD_PORT = 3306
+
+LOCAL_USER = "local_username"
+LOCAL_PASSWORD = "local_password"
+LOCAL_HOST = "localhost"
+LOCAL_DATABASE = "local_database_name"
+LOCAL_PORT = 3306
+
